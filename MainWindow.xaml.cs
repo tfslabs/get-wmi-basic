1c3a0e21e91b25176e46ac41d317b5d043da7680
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-08-24 21:50:53 +0700

    initial commit
    Co-Authored-By: Vo Luu Tuong Anh <anhvlt.2006@theflightsims.com>

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using GetWMIBasic.WMIMethods;
+using System.Linq;
+using System.Management;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GetWMIBasic
+{
+    public partial class MainWindow : Window
+    {
+        /*
+         *  Global properties
+         */
+        protected MachineMethods machine;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            machine = new MachineMethods();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Bottom_Label.Text = "Loading";
+            await Refresh();
+        }
+
+        /* 
+         * Button-based methods
+         */
+        private async void ConnectToAnotherComputer_ButtonAsync(object sender, RoutedEventArgs e)
+        {
+            ConnectForm connectForm = new ConnectForm();
+            machine = new MachineMethods(connectForm.ReturnValue());
+
+            await Refresh();
+        }
+
+        private void Exit_Button(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private async void Restart_ButtonAsync(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Do you want to restart the computer {machine.GetComputerName()}?", "Proceed?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    await machine.Connect("root\\cimv2");
+                    await machine.CallMethod("Win32_OperatingSystem", "*", "Win32Shutdown", new object[] { 6 });
+                }
+                catch (ManagementException ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private async void Shutdown_Button(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Do you want to shutdown the computer {machine.GetComputerName()}?", "Proceed?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    await machine.Connect("root\\cimv2");
+                    await machine.CallMethod("Win32_OperatingSystem", "*", "Win32Shutdown", new object[] { 5 });
+                }
+                catch (ManagementException ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private async void Refresh_ButtonAsync(object sender, RoutedEventArgs e)
+        {
+            await Refresh();
+        }
+
+        /*
+         * Non-button methods
+         */
+        private async Task Refresh()
+        {
+            try
+            {
+                await machine.Connect("root\\cimv2");
+
+                var biosProperties = machine.GetObjects("Win32_BIOS", "*");
+                foreach (ManagementObject biosProperty in (await biosProperties).Cast<ManagementObject>())
+                {
+                    BIOS_Manufacturer.Text = biosProperty["Manufacturer"]?.ToString() ?? string.Empty;
+                    BIOS_Name.Text = biosProperty["Name"]?.ToString() ?? string.Empty;
+                    BIOS_SN.Text = biosProperty["SerialNumber"]?.ToString() ?? string.Empty;
+                    BIOS_Version.Text = biosProperty["Version"]?.ToString() ?? string.Empty;
+                }
+
+                var osProperties = machine.GetObjects("Win32_ComputerSystem", "*");
+                foreach (ManagementObject osProperty in (await osProperties).Cast<ManagementObject>())
+                {
+                    Computer_Name.Text = osProperty["Name"]?.ToString() ?? string.Empty;
+                    Computer_Domain.Text = osProperty["Domain"]?.ToString() ?? string.Empty;
+                    Computer_Memory.Text = osProperty["TotalPhysicalMemory"]?.ToString() ?? string.Empty;
+                    Computer_SysType.Text = osProperty["SystemType"]?.ToString() ?? string.Empty;
+                }
+            }
+            catch (ManagementException ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ WMIMethods/MachineMethods.cs
@@ -0,0 +1,119 @@
+﻿using System.Linq;
+using System.Management;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace GetWMIBasic.WMIMethods
+{
+    public class MachineMethods
+    {
+        /*
+         * Class properties
+         */
+
+        protected (string computerName, string username, SecureString password) userCredential;
+        protected ConnectionOptions credential;
+        protected ManagementScope scope;
+        protected ManagementObjectSearcher searcher;
+
+        protected string scopePath;
+        protected bool isLocal;
+
+        /*
+         * Machine Method
+         * 1. Empty - Local
+         * 2. Or Connect through WMI
+         */
+
+        public MachineMethods()
+        {
+            userCredential.computerName = "localhost";
+            isLocal = true;
+            credential = new ConnectionOptions
+            {
+                Username = userCredential.username,
+                SecurePassword = userCredential.password,
+                Impersonation = ImpersonationLevel.Impersonate,
+                Authentication = AuthenticationLevel.PacketPrivacy,
+                EnablePrivileges = true
+            };
+        }
+
+        public MachineMethods((string computerName, string username, SecureString password) userCredential)
+        {
+            if (userCredential.computerName == "localhost")
+            {
+                userCredential.computerName = "localhost";
+                isLocal = true;
+                credential = new ConnectionOptions
+                {
+                    Username = userCredential.username,
+                    SecurePassword = userCredential.password,
+                    Impersonation = ImpersonationLevel.Impersonate,
+                    Authentication = AuthenticationLevel.PacketPrivacy,
+                    EnablePrivileges = true
+                };
+            }
+            else
+            {
+                this.userCredential.computerName = userCredential.computerName;
+                this.userCredential.username = userCredential.username;
+                this.userCredential.password = userCredential.password;
+                isLocal = false;
+                credential = new ConnectionOptions
+                {
+                    Username = userCredential.username,
+                    SecurePassword = userCredential.password,
+                    Impersonation = ImpersonationLevel.Impersonate,
+                    Authentication = AuthenticationLevel.PacketPrivacy,
+                    EnablePrivileges = true
+                };
+            }
+        }
+
+        /*
+         * Get value
+         */
+        public string GetComputerName()
+        {
+            return userCredential.computerName;
+        }
+
+        /*
+         * Connection method
+         */
+
+        public async Task Connect(string nameSpace)
+        {
+            scopePath = $"\\\\{userCredential.computerName}\\{nameSpace}";
+            scope = (isLocal) ? new ManagementScope(scopePath) : new ManagementScope(scopePath, credential);
+            await Task.Run(() => scope.Connect());
+        }
+
+        // Fix for CS1983 and CS1998: Change return type to Task<ManagementObjectCollection> and use await Task.Run(...) to make the method truly asynchronous.
+        public async Task<ManagementObjectCollection> GetObjects(string className, string fields)
+        {
+            ObjectQuery query = new ObjectQuery($"SELECT {fields} FROM {className}");
+            searcher = new ManagementObjectSearcher(scope, query);
+            return await Task.Run(() => searcher?.Get());
+        }
+
+        /*
+         * Call WMI Method
+         */
+        public async Task CallMethod(string className, string fields, string methodName, object[] args)
+        {
+            ObjectQuery query = new ObjectQuery($"SELECT {fields} FROM {className}");
+            await Task.Run(() =>
+            {
+                using (var searcher = new ManagementObjectSearcher(scope, query))
+                {
+                    foreach (ManagementObject manageObject in searcher.Get().Cast<ManagementObject>())
+                    {
+                        manageObject.InvokeMethod(methodName, args);
+                    }
+                }
+            });
+        }
+    }
+}
