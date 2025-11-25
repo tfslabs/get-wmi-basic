59d7d0c6accd77ecaba7eddb0bffb4c689d385f2
Shiroi Neko <shiroineko.tfs@theflightsims.com>
2025-11-26 06:50:08 +0700

    move to better namespace
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlt-2k6@users.noreply.github.com>

---
+++ DefaultUI/ConnectForm.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Security;
+using System.Windows;
+
+/* The Default UI includes
+ * 1. ConnectForm.xaml - A form to connect to a local or remote computer
+ * 2. ExceptionView.xaml - A window to handle exception messages
+ */
+namespace GetWMIBasic.DefaultUI
+{
+    /*
+     * Connection Form class
+     * It is used to get connection information from the user
+     * Expected behaviour:
+     *  1. User enter the computer name, username, and password to connect to a remote computer
+     *  2. The application tries verify the connection information
+     *  3. If what user enters is valid, the form closes and returns the information
+     */
+    public partial class ConnectForm : Window
+    {
+        /*
+         *  Global properties
+         *  isConnectLocally: A boolean to indicate whether the user wants to connect to the local computer
+         *          - false when the user wants to connect to a remote computer
+         *          - true when the user wants to connect to the local computer
+         */
+        private bool isConnectLocally;
+
+        // Constructor of the ConnectForm class
+        public ConnectForm()
+        {
+            // Set the default value of isConnectLocally to false
+            isConnectLocally = false;
+
+            // Initialize the components of the form
+            InitializeComponent();
+        }
+
+        // Button-based methods
+
+        /*
+         * Connect to External Button Click Event - The "Connect" button
+         * Validates the input fields and sets the DialogResult to true if valid
+         */
+        private void ConnetToExternal_Button(object sender, RoutedEventArgs e)
+        {
+            // Validate that none of the input fields are empty
+            if (
+                ComputerName_TextBox.Text.Length == 0 ||
+                UserName_TextBox.Text.Length == 0 ||
+                Password_TextBox.Password.Length == 0)
+            {
+                _ = MessageBox.Show("Please don't leave anything empty", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                // Close the form with a DialogResult of true
+                DialogResult = true;
+            }
+        }
+
+        /*
+         * Connect to Local Button Click Event - The "Connect to Local" button
+         * Sets the isConnectLocally flag to true and sets the DialogResult to true
+         */
+        private void ConnectToLocal_Button(object sender, RoutedEventArgs e)
+        {
+            isConnectLocally = true;
+            DialogResult = true;
+        }
+
+        /*
+         * Cancel Button Click Event - The "Cancel" button
+         * Closes the form without setting a DialogResult
+         */
+        private void Cancel_Button(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        // Non-button methods
+
+        /*
+         * Return Value Method
+         * Returns a tuple containing the computer name, username, and password as a SecureString
+         */
+        public (string, string, SecureString) ReturnValue()
+        {
+            // Show the dialog and wait for user interaction
+            _ = ShowDialog();
+
+            // Prepare the return values based on whether connecting locally or remotely
+            string computerName = (isConnectLocally) ? "localhost" : ComputerName_TextBox.Text;
+            string userName = (isConnectLocally) ? "" : UserName_TextBox.Text;
+            SecureString password = new SecureString();
+
+            // Only populate the password if connecting remotely
+            if (!isConnectLocally)
+            {
+                foreach (char c in Password_TextBox.Password)
+                {
+                    password.AppendChar(c);
+                }
+            }
+
+            // Return the collected values as a tuple
+            return (computerName, userName, password);
+        }
+    }
+}
---
+++ DefaultUI/ExceptionView.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Windows;
+using GetWMIBasic.WMIMethods;
+
+/* The Default UI includes
+ * 1. ConnectForm.xaml - A form to connect to a local or remote computer
+ * 2. ExceptionView.xaml - A window to handle exception messages
+ */
+namespace GetWMIBasic.DefaultUI
+{
+    /*
+     * Exception View class
+     * It is used to handle exception messages
+     * 
+     * Excepted behaviour:
+     *  1. An exception is passed to the HandleException method
+     *  2. If the exception is of an allowed type, show the exception message 
+     *      and give the user the option to ignore it; else, force close the application
+     */
+    public partial class ExceptionView : Window
+    {
+        /*
+         * Global properties
+         *  allowedExceptionTypes: A list of exception types that are allowed to be ignored
+         */
+        private readonly HashSet<Type> allowedExceptionTypes = new HashSet<Type>
+        {
+            typeof(UnauthorizedAccessException),
+            typeof(COMException),
+            typeof(ManagementException),
+            typeof(CommonException)
+        };
+
+        // Constructor of the Exception View class
+        public ExceptionView()
+        {
+            InitializeComponent();
+        }
+
+        // Button-based methods
+
+        /*
+         * Click Ignore Exception Button - The "Ignore" button
+         * Let user ignore the exception and continue using the application
+         */
+        private void Click_IgnoreException(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /*
+         * Click Close Application Button - The "Close Application" button
+         * Close the application in case of a critical or unhandled exception
+         */
+        private void Click_CloseApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Non-button methods
+
+        /*
+         * Handle Exception Method
+         * Get the exception info, and decide whether to allow the user to ignore it
+         */
+        public void HandleException(Exception e)
+        {
+            // If the exception is null, ignore it
+            if (e == null)
+            {
+                return;
+            }
+
+            // Check if the exception type is in the allowed list
+            bool isAllowed = this.allowedExceptionTypes.Any(t => t.IsInstanceOfType(e));
+
+            // If not allowed, change the message and hide the ignore button
+            if (!isAllowed)
+            {
+                // Reset the exception message
+                ExceptionMessage_TextBlock.Text = "An unexpected error has occurred.\n" +
+                    "You should close the application and check for the system";
+                // Hide the ignore button
+                Button_Ignore.Visibility = Visibility.Collapsed;
+            }
+
+            // Show the detailed exception info
+#if !DEBUG
+            DetailedExceptionDetail_TextBox.Text = e.Message;
+#else
+            DetailedExceptionDetail_TextBox.Text = e.ToString();
+#endif
+
+            // Show the exception view window
+            ShowDialog();
+        }
+    }
+}
