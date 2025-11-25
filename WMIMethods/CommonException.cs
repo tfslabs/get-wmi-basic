e7bad701eee60add3935cff07bca5299b6b1de01
Shiroi Neko <shiroineko.tfs@theflightsims.com>
2025-11-26 06:50:36 +0700

    exceptions handlers
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlt-2k6@users.noreply.github.com>

---
+++ WMIMethods/CommonException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * The default WMI method class includes:
+ *  1. CriticalException.cs - A custom exception class for critical WMI errors
+ *  2. CommonException.cs - A custom exception class for common WMI errors
+ *  3. MachineMethods.cs - A base class for WMI operations on local and remote machines
+ */
+namespace GetWMIBasic.WMIMethods
+{
+    /*
+     * Common Exception class
+     * It is used to test common exceptions in WMI operations
+     */
+    public class CommonException : SystemException
+    {
+        // Default constructor - No custom message (use default message)
+        public CommonException()
+        {
+            // Throw a new exception with a default message
+            throw new Exception("This is a custom exception for WMI-related errors.");
+        }
+
+        // Default constructor - with custom message
+        public CommonException(string message) : base(message)
+        {
+            // Throw a new exception with custom message
+            throw new Exception(message);
+        }
+    }
+}
---
+++ WMIMethods/CriticalException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * The default WMI method class includes:
+ *  1. CriticalException.cs - A custom exception class for critical WMI errors
+ *  2. CommonException.cs - A custom exception class for common WMI errors
+ *  3. MachineMethods.cs - A base class for WMI operations on local and remote machines
+ */
+namespace GetWMIBasic.WMIMethods
+{
+    /*
+     * Critical Exception class
+     * It is used to test critical exceptions in WMI operations
+     */
+    public class CriticalException : SystemException
+    {
+        // Default constructor - No custom message (use default message)
+        public CriticalException()
+        {
+            // Throw a new exception with a default message
+            throw new Exception("A critical exception has occurred in the WMI operations.");
+        }
+
+        // Default constructor - with custom message
+        public CriticalException(string message) : base(message)
+        {
+            // Throw a new exception with custom message
+            throw new Exception(message);
+        }
+    }
+}
